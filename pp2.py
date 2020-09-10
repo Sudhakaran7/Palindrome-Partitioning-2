517c49139ff5ebdcd3bffcb339b811d6c8555eb6
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-09-10 09:43:12 +0700

    Create pp2.py

---
+++ pp2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minCut(self, s):
+        dp = [0 for i in range(len(s)+1)]
+        p = [[False for i in range(len(s))] for j in range(len(s))]
+        for i in range(len(s)+1):
+            dp[i] = len(s) - i
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i] == s[j] and (((j - i) < 2) or p[i+1][j-1]):
+                    p[i][j] = True
+                    dp[i] = min(1+dp[j+1], dp[i])
+        return dp[0]-1
+val=Solution()
+s=input()
+print(val.minCut(s))
